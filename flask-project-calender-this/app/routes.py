87ce78fb35cee68fb31f9a331684ba5971e239b1
jagLas <jglasher@gmail.com>
2023-07-21 16:19:32 -0400

    created postgresdb and connection

--- flask-project-calender-this/app/routes.py
+++ flask-project-calender-this/app/routes.py
@@ -1,7 +1,23 @@
 from flask import Blueprint, render_template
+import psycopg2
+import os
 
 bp = Blueprint('main', __name__, '/')
 
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
+
 @bp.route('/')
 def main():
-    return render_template('main.html')
+    conn = psycopg2.connect(**CONNECTION_PARAMETERS)
+    cursor = conn.cursor()
+    print(cursor)
+    cursor.execute('SELECT * FROM appointments')
+    records = cursor.fetchall()
+    return records
+    
+    # return render_template('main.html', rows=records)
