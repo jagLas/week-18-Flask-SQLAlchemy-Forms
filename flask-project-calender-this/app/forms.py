4f93c2a09f41f9779e51cb97e8eca37f2b96564e
jagLas <jglasher@gmail.com>
2023-07-31 14:16:21 -0400

    added custom validation for end time

--- flask-project-calender-this/app/forms.py
+++ flask-project-calender-this/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, DateField, TimeField, TextAreaField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from datetime import datetime
 
 class AppointmentForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -11,3 +12,11 @@ class AppointmentForm(FlaskForm):
     description = TextAreaField('Description', validators=[DataRequired()])
     private = BooleanField('Private?')
     submit = SubmitField('Create An Appointment')
+
+    def validate_end_datetime_date(form, field):
+        start_date = datetime.combine(form.start_datetime_date.data, form.start_datetime_time.data)
+        end_date = datetime.combine(field.data, form.end_datetime_time.data)
+
+        if start_date >= end_date:
+            msg = 'End date/time must come after start date/time'
+            raise ValidationError(msg)
