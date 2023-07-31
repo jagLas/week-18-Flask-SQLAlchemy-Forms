52e5210fb1aa6ec4df785da63c5a84c923f4c812
jagLas <jglasher@gmail.com>
2023-07-31 12:41:57 -0400

    added validators

--- flask-project-calender-this/app/forms.py
+++ flask-project-calender-this/app/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, DateField, TimeField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired
 
 class AppointmentForm(FlaskForm):
-    name = StringField('Name')
-    start_datetime_date = DateField('Start Date')
-    start_datetime_time = TimeField('Start Time')
-    end_datetime_date = DateField('End Date')
-    end_datetime_time = TimeField('End Time')
-    description = TextAreaField('Description')
+    name = StringField('Name', validators=[DataRequired()])
+    start_datetime_date = DateField('Start Date', validators=[DataRequired()])
+    start_datetime_time = TimeField('Start Time', validators=[DataRequired()])
+    end_datetime_date = DateField('End Date', validators=[DataRequired()])
+    end_datetime_time = TimeField('End Time', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
     private = BooleanField('Private?')
     submit = SubmitField('Create An Appointment')
