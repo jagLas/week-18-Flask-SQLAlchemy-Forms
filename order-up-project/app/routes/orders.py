9095ed3b5d5eb6204c8dae24cb3782595d0bcbb5
jagLas <jglasher@gmail.com>
2023-08-03 19:17:05 -0400

    open order route and button

--- order-up-project/app/routes/orders.py
+++ order-up-project/app/routes/orders.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
+from sqlalchemy.orm import joinedload
 from flask_login import login_required
-from ..models import db, Order, OrderDetail
+from ..models import db, Order, OrderDetail, Table
 
 bp = Blueprint('orders', __name__, url_prefix='')
 
@@ -8,7 +9,7 @@ bp = Blueprint('orders', __name__, url_prefix='')
 @bp.route('/')
 @login_required
 def index():
-    orders = Order.query.all()
+    orders = Order.query.options(joinedload(Order.table)).all()
     return render_template('orders.html', orders=orders)
 
 
@@ -17,6 +18,16 @@ def index():
 def close_table(id):
     order = db.get_or_404(Order, id)
     order.finished = True
+    db.session.commit()
+
+    return redirect(url_for('orders.index'))
+
+
+@bp.route('/<int:id>/open', methods=['POST'])
+@login_required
+def open_table(id):
+    order = db.get_or_404(Order, id)
+    order.finished = False
     db.session.commit()
 
     return redirect(url_for('orders.index'))
