2fa0f1e143c0da7fdb36b6e0c623f7d50d22c1e9
jagLas <jglasher@gmail.com>
2023-08-03 11:12:18 -0400

    added menu models

--- order-up-project/database.py
+++ order-up-project/database.py
@@ -6,7 +6,7 @@ load_dotenv()
 # so that the environment variables are
 # properly loaded.
 from app import app, db
-from app.models import Employee
+from app.models import Employee, Menu, MenuItem, MenuItemType
 
 
 with app.app_context():
@@ -15,4 +15,17 @@ with app.app_context():
 
     employee = Employee(name="Margot", employee_number=1234, password="password")
     db.session.add(employee)
+    db.session.commit()
+
+    beverages = MenuItemType(name="Beverages")
+    entrees = MenuItemType(name="Entrees")
+    sides = MenuItemType(name="Sides")
+
+    dinner = Menu(name="Dinner")
+
+    fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
+    drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
+    jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+    # db.session.add(beverages)
+    db.session.add_all([beverages, entrees, sides, dinner, fries, drp, jambalaya])
     db.session.commit()
