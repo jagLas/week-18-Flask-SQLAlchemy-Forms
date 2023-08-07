5ef104ca8757d5f6d5e0fff762744bf3a8b9f2c2
jagLas <jglasher@gmail.com>
2023-08-07 13:14:28 -0400

    form created

--- package-tracker-project/app/__init__.py
+++ package-tracker-project/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from app.config import Config
+from app.shipping_form import ShippingForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +13,6 @@ def root():
 
 @app.route('/new_package', methods=['POST', 'GET'])
 def new_package():
-    return render_template('shipping_request.html')
+    form = ShippingForm()
+
+    return render_template('shipping_request.html', form=form)
--- package-tracker-project/app/shipping_form.py
+++ package-tracker-project/app/shipping_form.py
@@ -1,11 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from map.map import map
 
+cities_list = tuple(map)
 
-class shipping_form(FlaskForm):
-    sender_name = StringField('Sender Name')
-    recipient_name = StringField('Recipient Name')
-    origin = SelectField('Origin')
-    destination = SelectField('Destination')
-    express = BooleanField('Express Shipping')
+print(cities_list)
+
+class ShippingForm(FlaskForm):
+    sender = StringField('Sender Name', validators=[DataRequired()])
+    recipient = StringField('Recipient Name', validators=[DataRequired()])
+    origin = SelectField('Origin', choices=cities_list, validators=[DataRequired()])
+    destination = SelectField('Destination', choices=cities_list, validators=[DataRequired()])
+    express = BooleanField('Express Shipping',)
     submit = SubmitField('Submit')
