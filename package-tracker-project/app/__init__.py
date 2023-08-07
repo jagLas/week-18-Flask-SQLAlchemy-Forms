3460b4ae3f275f21e94af0972a0850d50257d867
jagLas <jglasher@gmail.com>
2023-08-07 13:50:58 -0400

    sqlalchemy and alembic setup

--- package-tracker-project/app/__init__.py
+++ package-tracker-project/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from app.config import Config
 from app.shipping_form import ShippingForm
+from flask_migrate import Migrate
+from app.models import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
